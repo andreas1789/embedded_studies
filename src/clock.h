bc6f0e744171fc27b2df5b942f2435fd73ad68eb
Andreas Hoffmann <36099258+andreas1789@users.noreply.github.com>
2023-09-19 22:27:23 +0200

    pll clock

--- src/clock.h
+++ src/clock.h
@@ -5,9 +5,27 @@ typedef unsigned int uint32_t;
 #define RCC_BASE 0x40021000
 
 #define RCC_CR (*(volatile uint32_t *)(RCC_BASE + 0x0))
+#define RCC_CR_HSE_ON (0x1 << 16)
+#define RCC_CR_HSE_RDY (0x1 << 17)
+#define RCC_CR_PLLON (0x1 << 24)
+#define RCC_CR_PLLRDY (0x1 << 25)
 
 #define RCC_APB2ENR (*(volatile uint32_t *)(RCC_BASE + 0x18))
 #define RCC_APB2ENR_IOPCEN (1 << 4)
+
+#define RCC_CFGR (*(volatile uint32_t *)(RCC_BASE + 0x4))
+#define RCC_CFGR_PPRE1_MSK (0x7 << 8)
+#define RCC_CFGR_PPRE1_2 (0x4)
+#define RCC_CFGR_PLLXTPRE_MSK (0x1 << 17)
+#define RCC_CFGR_PLLXTPRE_HSE 0
+#define RCC_CFGR_PLLSRC_MSK (0x1 << 16) 
+#define RCC_CFGR_PLLSRC_HSE 0 
+#define RCC_CFGR_PLLMUL_MSK (0xf << 18)
+#define RCC_CFGR_PLLMUL_9 (0x7 << 18)
+#define RCC_CFGR_PLLMUL_4 (0x2 << 18)
+#define RCC_CFGR_SW_MSK 0x3
+#define RCC_CFGR_SW_PLL 0x2
+#define RCC_CFGR_SWS_PLL (0x2 << 2)
 
 
 // General Purpose IO C - GPIOC
@@ -23,3 +41,9 @@ typedef unsigned int uint32_t;
 
 #define GPIOC_ODR (*(volatile uint32_t *)(GPIOC_BASE + 0xC))
 #define GPIOC_ODR_PIN(x) (0x1 << x)
+
+// Flash ACR
+#define FLASH_ACR_BASE 0x40022000
+
+#define FLASH_ACR (*(volatile uint32_t *)(FLASH_ACR_BASE + 0x0))
+#define FLASH_ACR_LATENCY_TWO 0x2
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,59 @@
 #include "clock.h"
 
+void init_pll();
+void init_peripheral_clock();
+
 void SystemInit()
 {
-    // Enable clock on gpio ports
+    init_pll();
+    init_peripheral_clock();
+}
+
+void init_peripheral_clock()
+{    // Enable clock on gpio ports
     RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
 
     // configure gpio port c as output
     GPIOC_CRH &= ~GPIOC_CRH_MODE_MSK(13);
     GPIOC_CRH |= GPIOC_CRH_MODE_OUTPUT(13);
+}
+
+void init_pll()
+{
+    // Set Flash wait state
+    FLASH_ACR |= FLASH_ACR_LATENCY_TWO;
+
+    // Set APB divisor to 2
+    RCC_CFGR &= ~RCC_CFGR_PPRE1_MSK;
+    RCC_CFGR |= RCC_CFGR_PPRE1_2;
+
+    // Set divisor before PLL to no division
+    RCC_CFGR &= ~RCC_CFGR_PLLXTPRE_MSK;
+    RCC_CFGR |= RCC_CFGR_PLLXTPRE_HSE;
+
+    // Activate HSE and set as source for PLL
+    RCC_CR |= RCC_CR_HSE_ON;
+    while (!(RCC_CR & RCC_CR_HSE_RDY))
+    {}
+
+    RCC_CFGR &= ~RCC_CFGR_PLLSRC_MSK;
+    RCC_CFGR |= RCC_CFGR_PLLSRC_HSE;
+
+    // Set PLL multiplication
+    RCC_CFGR &= ~RCC_CFGR_PLLMUL_MSK;
+    RCC_CFGR |= RCC_CFGR_PLLMUL_9;
+
+    // Activate PLL
+    RCC_CR |= RCC_CR_PLLON;
+    while (!(RCC_CR & RCC_CR_PLLRDY))
+    {}
+
+    // Set clock to PLL
+    RCC_CFGR &= ~RCC_CFGR_SW_MSK;
+    RCC_CFGR |= RCC_CFGR_SW_PLL;
+    while (!(RCC_CFGR & RCC_CFGR_SWS_PLL))
+    {}
+
 }
 
 int main()
