a5c5bc8c26bbcdf8f983483b60957e8c99e28d94
Andreas Hoffmann <36099258+andreas1789@users.noreply.github.com>
2023-10-31 22:54:51 +0100

    btld jumping to app

---
+++ bootloader/src/main.cpp
@@ -0,0 +1,105 @@
+#include "../../application/include/types.hpp"
+#include "../../application//include/clock.hpp"
+#include "../../application//include/gpio.hpp"
+
+using namespace types;
+
+void init_core_clocks();
+void init_peripheral_clocks();
+void init_array();
+void init_hw();
+
+extern void (*_spreinit_array []) (void) __attribute__((weak));
+extern void (*_sinit_array []) (void) __attribute__((weak));
+extern void (*_epreinit_array []) (void) __attribute__((weak));
+extern void (*_einit_array []) (void) __attribute__((weak));
+
+void SystemInit()
+{
+    init_core_clocks();
+    init_peripheral_clocks();
+    init_array();
+}
+
+void init_peripheral_clocks()
+{   
+}
+
+void init_core_clocks()
+{
+    // Set Flash wait state
+    FLASH_ACR |= FLASH_ACR_LATENCY_TWO;
+
+    // Set APB divisor to 2
+    RCC_CFGR &= ~RCC_CFGR_PPRE1_MSK;
+    RCC_CFGR |= RCC_CFGR_PPRE1_2;
+
+    // Set divisor before PLL to no division
+    RCC_CFGR &= ~RCC_CFGR_PLLXTPRE_MSK;
+    RCC_CFGR |= RCC_CFGR_PLLXTPRE_HSE;
+
+    // Activate HSE and set as source for PLL
+    RCC_CR |= RCC_CR_HSE_ON;
+    while (!(RCC_CR & RCC_CR_HSE_RDY))
+    {}
+
+    RCC_CFGR &= ~RCC_CFGR_PLLSRC_MSK;
+    RCC_CFGR |= RCC_CFGR_PLLSRC_HSE;
+
+    // Set PLL multiplication
+    RCC_CFGR &= ~RCC_CFGR_PLLMUL_MSK;
+    RCC_CFGR |= RCC_CFGR_PLLMUL_9;
+
+    // Activate PLL
+    RCC_CR |= RCC_CR_PLLON;
+    while (!(RCC_CR & RCC_CR_PLLRDY))
+    {}
+
+    // Set clock to PLL
+    RCC_CFGR &= ~RCC_CFGR_SW_MSK;
+    RCC_CFGR |= RCC_CFGR_SW_PLL;
+    while (!(RCC_CFGR & RCC_CFGR_SWS_PLL))
+    {}
+
+}
+
+void init_array()
+{
+    uint32_t size;
+    size = &(_einit_array[0]) - &(_sinit_array[0]);
+    for(uint32_t i=0U; i<size; ++i)
+    {
+        _sinit_array[i]();
+    }
+    
+    size = &(_epreinit_array[0]) - &(_spreinit_array[0]);
+    for(uint32_t i=0U; i<size; ++i)
+    {
+        _spreinit_array[i]();
+    }
+}
+
+
+static void  __attribute__((naked)) start_application(uint32_t pc, uint32_t sp)
+{
+    __asm("                                             \n\
+            msr msp, r1 /* load main stack pointer */   \n\
+            bx r0 /* load program counter */            \n\
+    ");
+
+    (void)pc;
+    (void)sp;
+}
+
+int main()
+{
+    //(void)start_application;
+    uint32_t *app_code = (uint32_t *)0x08002000;
+    uint32_t app_sp = app_code[0];
+    uint32_t app_pc = app_code[1];
+    start_application(app_pc, app_sp);
+
+    while(1) {}
+
+    return 0;
+}
---
+++ create_bin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the start address of the application flash region
+bootloader_flash_start=0x08000000
+application_flash_start=0x08002000
+
+# Binary file names
+bootloader_file="gen_btld/stm32f103c8t6_btld.bin"
+application_file="gen_app/stm32f103c8t6_app.bin"
+combined_file="combined.bin"
+
+# Calculate the required padding size
+bootloader_size=$(wc -c < "$bootloader_file")
+padding_size=$((application_flash_start - bootloader_size - bootloader_flash_start))
+
+# Generate a padding binary file
+padding_file=$(mktemp)
+truncate -s $padding_size $padding_file
+
+# Concatenate the bootloader binary, padding, and application binary
+cat $bootloader_file $padding_file $application_file > $combined_file
+
+# Clean up the temporary padding file
+rm -f $padding_file
